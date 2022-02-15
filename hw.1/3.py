fc6bbdff2cb85e2f39c31248f5c3b17e9c841658
enns <yuliya_enns@mail.ru>
2022-02-15 17:47:43 +0800

    homework 1 (task 1-7)

---
+++ hw.1/3.py
@@ -0,0 +1,10 @@
+#Узнайте у пользователя число n. Найдите сумму чисел n + nn + nnn.
+# Например, пользователь ввёл число 3. Считаем 3 + 33 + 333 = 369.
+
+n = input('Введите число ')
+result_2 = n + n
+result_3 = n + n + n
+#print(n)
+#print(result_2)
+#print(result_3)
+namber = print(int(n) + int(result_2) + int(result_3))
